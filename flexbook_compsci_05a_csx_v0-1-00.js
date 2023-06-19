1f2c70d2c731a61c2cde7570b0a7bc8e4525d0fc
Paulo <pauloalvimottoni@gmail.com>
2023-06-19 16:00:03 -0300

    estilo e exemplo para códigos

--- flexbook_compsci_05a_csx_v0-1-00.js
+++ flexbook_compsci_05a_csx_v0-1-00.js
@@ -648,7 +648,7 @@ let livro_csx ={
 
 		  {ordem:2,
 		  predecessor:1,
-		  texto:'As condições podem se basear em qualquer informação conhecida. Por exemplo, regras para parar ou prosseguir com o carro dependem dos sinais de trânsito.<p><code>Se "o sinal estiver vermelho" então "pare".<br/>Se "o sinal estiver verde" então "prossiga". Se "o sinal estiver amarelo" então Se "já estiver passando sob o sinal" então "prossiga" senão "pare".</code>',
+		  texto:'As condições podem se basear em qualquer informação conhecida. Por exemplo, regras para parar ou prosseguir com o carro dependem dos sinais de trânsito.<p><code>Se "o sinal estiver vermelho" então "pare"<br/>Se "o sinal estiver verde" então "prossiga"<br/>Se "o sinal estiver amarelo" então<br/>&nbsp;&nbsp;&nbsp;&nbsp;Se "já estiver passando sob o sinal"<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;então "prossiga"<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;senão "pare".</code>',
 		  urlAudioTexto:'',
 		  textoSimplificado:'Condições',
 		  textoImpresso:1,
